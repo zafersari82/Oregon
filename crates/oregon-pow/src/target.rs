77ee893610760c5008aa5c08bee051cb368be2e4
zafersari82 <zafersari82@gmail.com>
2026-09-03 10:01:03 +0300

    mutation: reverse Oregon PoW target byte order

--- crates/oregon-pow/src/target.rs
+++ crates/oregon-pow/src/target.rs
@@ -1,7 +1,7 @@
 use core::cmp::Ordering;
 
 pub fn hash_meets_target(hash_le: [u8; 32], target_le: [u8; 32]) -> bool {
-    for index in (0..32).rev() {
+    for index in 0..32 {
         match hash_le[index].cmp(&target_le[index]) {
             Ordering::Less => return true,
             Ordering::Greater => return false,
